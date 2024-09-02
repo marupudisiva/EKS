af7bf510e25f7a9b1981f4e3d5cc8946aa9f7719
marupudisiva <marupudisiva@gmail.com>
2024-09-02 18:30:15 +0530

    fifth commit

--- eks/eks.tf
+++ eks/eks.tf
@@ -1,9 +1,9 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.3.0"
+  version = "latest"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.22"
+  cluster_version = "1.24"
   subnets         = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
