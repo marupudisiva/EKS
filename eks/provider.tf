8386d41c5c6fe32d8cedcb582fdedc49ff5c3914
marupudisiva <marupudisiva@gmail.com>
2024-09-02 20:33:46 +0530

    19 commit

--- eks/provider.tf
+++ eks/provider.tf
@@ -1,3 +1,5 @@
 provider "aws" {
   region = var.region
+  access_key = var.aws_access_key # optional if using environment variables
+  secret_key = var.aws_secret_key # optional if using environment variables
 }
