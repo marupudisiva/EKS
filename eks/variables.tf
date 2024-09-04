aeec732dc5d430695c748dc7defec9fd8a50922f
marupudisiva <marupudisiva@gmail.com>
2024-09-04 15:30:54 +0530

    32 commit

--- eks/variables.tf
+++ eks/variables.tf
@@ -71,14 +71,14 @@ variable "tags" {
 # aws-auth configmap
 ################################################################################
 
-variable "manage_aws_auth_configmap" {
-  description = "Determines whether to manage the aws-auth configmap"
-  type        = bool
-  default     = false
-}
+# variable "manage_aws_auth_configmap" {
+#   description = "Determines whether to manage the aws-auth configmap"
+#   type        = bool
+#   default     = false
+# }
 
-variable "create_aws_auth_configmap" {
-  description = "Determines whether to create the aws-auth configmap. NOTE - this is only intended for scenarios where the configmap does not exist (i.e. - when using only self-managed node groups). Most users should use `manage_aws_auth_configmap`"
-  type        = bool
-  default     = false
-}
+# variable "create_aws_auth_configmap" {
+#   description = "Determines whether to create the aws-auth configmap. NOTE - this is only intended for scenarios where the configmap does not exist (i.e. - when using only self-managed node groups). Most users should use `manage_aws_auth_configmap`"
+#   type        = bool
+#   default     = false
+# }
